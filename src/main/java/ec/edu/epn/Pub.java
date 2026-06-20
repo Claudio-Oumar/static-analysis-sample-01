9dd1364c6c87cead2a17d3b3de9ae3b0a277f343
Claudio-Oumar <claudioomar2010abc@gmail.com>
2026-06-20 17:18:08 -0500

    Correccion con SonarQube

--- src/main/java/ec/edu/epn/Pub.java
+++ src/main/java/ec/edu/epn/Pub.java
@@ -9,59 +9,42 @@ public class Pub {
 
     public int computeCost(String drink, boolean student, int amount) {
 
-        if (amount > 2 && (drink == GT || drink == BACARDI_SPECIAL)) {
-            throw new RuntimeException("Too many drinks, max 2.");
+        if (amount > 2 && (GT.equals(drink) || BACARDI_SPECIAL.equals(drink))) {
+            throw new IllegalArgumentException("Too many drinks, max 2.");
         }
         int price;
-        if (drink.equals(ONE_BEER)) {
+
+        if (ONE_BEER.equals(drink)) {
             price = 74;
         }
-        else if (drink.equals(ONE_CIDER)) {
+        else if (ONE_CIDER.equals(drink)) {
             price = 103;
         }
-        else if (drink.equals(A_PROPER_CIDER)) price = 110;
-        else if (drink.equals(GT)) {
-            price = ingredient6() + ingredient5() + ingredient4();
+        else if (A_PROPER_CIDER.equals(drink)) {
+            price = 110;
         }
-        else if (drink.equals(BACARDI_SPECIAL)) {
-            price = ingredient6()/2 + ingredient1() + ingredient2() + ingredient3();
+
+        else if (GT.equals(drink)) {
+            price = GIN_UNIT_PRICE + TONIC_WATER_UNIT_PRICE + GREEN_STUFF_UNIT_PRICE;
+        }
+        else if (BACARDI_SPECIAL.equals(drink)) {
+            price = GIN_UNIT_PRICE / 2 + RUM_UNIT_PRICE + GRENADINE_UNIT_PRICE + LIME_JUICE_UNIT_PRICE;
         }
         else {
-            throw new RuntimeException("No such drink exists");
+            throw new IllegalArgumentException("No such drink exists");
         }
-        if (student && (drink == ONE_CIDER || drink == ONE_BEER || drink == A_PROPER_CIDER)) {
-            price = price - price/10;
+        if (student && (ONE_CIDER.equals(drink) || ONE_BEER.equals(drink) || A_PROPER_CIDER.equals(drink))) {
+            price = price - price / 10;
         }
         return price*amount;
     }
 
     //one unit of rum
-    private int ingredient1() {
-        return 65;
-    }
+    private static final int RUM_UNIT_PRICE = 65;
+    private static final int GRENADINE_UNIT_PRICE = 10;
+    private static final int LIME_JUICE_UNIT_PRICE = 10;
+    private static final int GREEN_STUFF_UNIT_PRICE = 10;
+    private static final int TONIC_WATER_UNIT_PRICE = 20;
+    private static final int GIN_UNIT_PRICE = 85;
 
-    //one unit of grenadine
-    private int ingredient2() {
-        return 10;
-    }
-
-    //one unit of lime juice
-    private int ingredient3() {
-        return 10;
-    }
-    
-    //one unit of green stuff
-    private int ingredient4() {
-        return 10;
-    }
-
-    //one unit of tonic water
-    private int ingredient5() {
-        return 20;
-    }
-
-    //one unit of gin
-    private int ingredient6() {
-        return 85;
-    }
 }
